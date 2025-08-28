0285ad0ded1d686650c22f1fe3c71b0a7308937b
Dev_Num0 <106236672+ApostleDev0@users.noreply.github.com>
2025-08-28 16:22:52 +0700

    Update Logic Projectile

--- Cosmic Survival/Assets/_CosmicSurvival/Scripts/Projectile/ProjectileController.cs
+++ Cosmic Survival/Assets/_CosmicSurvival/Scripts/Projectile/ProjectileController.cs
@@ -10,17 +10,22 @@ public class ProjectileController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        // goi toi Rigidbody2D cua dan roi gan van toc cho no
-        // transform.up la 1 vector chi huong len tren (0,1)
-        GetComponent<Rigidbody2D>().velocity = transform.up * speed;
-
-        // vien dan se bi huy sau khoang thoi gian
-        Destroy(gameObject, 2f);
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // transform.Translate la ham cua Unity
+        // di chuyen doi tuong theo 1 vector va toc do dc cung cap
+        // Vector2.up la vector luon huong len tren
+        // speed * Time.deltaTime = quang duong di duoc trong 1 khung hinh
+        transform.Translate(Vector2.up *  speed * Time.deltaTime);
+
+        // ham tu huy khi ra khoi map
+        if(transform.position.y > 10f)
+        {
+            Destroy(gameObject);
+        }
     }
 }
